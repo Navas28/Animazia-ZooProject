b0aaa68900e842986e46113e0f330c22628e41a4
Navas . M <navasmhd79@gmail.com>
2025-03-07 18:15:37 +0530

    about, features completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import About from "./components/About";
+import Features from "./components/Features";
+
 
 const App = () => {
+    useEffect(() => {
+        AOS.init({
+          duration: 1000, // Animation duration (in ms)
+          once: false,     // Animation runs once (true/false)
+        });
+      }, []);      
     return (
         <div className="w-full overflow-hidden">
             <Header />
+            <About/>
+            <Features/>
         </div>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
-            window.scrollY > 50 ? setStickyNavbar(true) : setStickyNavbar(false);
+            window.scrollY > 600 ? setStickyNavbar(true) : setStickyNavbar(false);
         });
     }, []);
 
@@ -19,51 +19,51 @@ const Navbar = () => {
     return (
         <nav
             className={`w-full px-5 py-5 fixed top-0 left-0 flex items-center justify-between z-10  ${
-                stickyNavbar ? "bg-blackBean transition-all duration-500" : "bg-transparent"
+                stickyNavbar ? "bg-customeWhite transition-all ease-in duration-500" : "bg-transparent"
             }`}
         >
-            <img src={logoWhite} alt="logo" className="w-30 sm:w-[180px]" />
+            <img src={stickyNavbar ? logoGreen : logoWhite} alt="logo" className="w-30 sm:w-[180px]" />
             <ul
                 className={`sm:static sm:flex sm:z-auto sm:w-auto sm:bg-transparent sm:pt-0 sm:text-customeWhite font-para fixed top-0 right-0 bottom-0 bg-customeWhite cursor-pointer text-customeGreen z-10 w-48 pt-[70px] transition-all duration-500 ${
                     menuIcon ? "right-0" : "right-[-100%]"
                 }`}
             >
-                <a
+                <a 
                     href="#Home"
                     onClick={toggleMenu}
-                    className="block my-10 mx-6 sm:inline-block sm:my-1.5 sm:mx-3 lg:mx-5 text-lg"
+                    className={`block my-10 mx-6 sm:inline-block sm:my-1.5 sm:mx-3 lg:mx-5 text-lg ${stickyNavbar ? 'text-black': 'sm:text-white'}`}
                 >
                     Home
                 </a>
                 <a
                     href="#About"
                     onClick={toggleMenu}
-                    className="block my-10 mx-6 sm:inline-block sm:my-1.5 sm:mx-3 lg:mx-5 text-lg"
+                    className={`block my-10 mx-6 sm:inline-block sm:my-1.5 sm:mx-3 lg:mx-5 text-lg ${stickyNavbar ? 'text-black': 'sm:text-white'}`}
                 >
                     About
                 </a>
                 <a
-                    href="#Service"
+                    href="#Features"
                     onClick={toggleMenu}
-                    className="block my-10 mx-6 sm:inline-block sm:my-1.5 sm:mx-3 lg:mx-5 text-lg"
+                    className={`block my-10 mx-6 sm:inline-block sm:my-1.5 sm:mx-3 lg:mx-5 text-lg ${stickyNavbar ? 'text-black': 'sm:text-white'}`}
                 >
-                    Service
+                    Features
                 </a>
                 <a
                     href="#Contact"
                     onClick={toggleMenu}
-                    className="block my-10 mx-6 sm:inline-block sm:my-1.5 sm:mx-3 lg:mx-5 text-lg"
+                    className={`block my-10 mx-6 sm:inline-block sm:my-1.5 sm:mx-3 lg:mx-5 text-lg ${stickyNavbar ? 'text-black': 'sm:text-white'}`}
                 >
                     Contact
                 </a>
                 <a onClick={toggleMenu} className="block my-10 mx-6 sm:inline-block sm:my-1.5 sm:mx-3 lg:mx-5 text-lg">
-                    <button className="text-customeWhite sm:text-customeGreen bg-customeGreen sm:bg-customeWhite text-md rounded-md cursor-pointer border-0 outline-0 px-4">
+                    <button className="text-customeWhite sm:text-customeGreen bg-customeGreen sm:bg-customeWhite text-md rounded-md cursor-pointer border border-customeGreen outline-0 px-4">
                         Donate
                     </button>
                 </a>
                 <IoCloseSharp onClick={toggleMenu} className="sm:hidden cursor-pointer text-4xl absolute top-9 right-5" />
             </ul>
-            <TiThMenuOutline onClick={toggleMenu} className="block sm:hidden text-white cursor-pointer text-2xl" />
+            <TiThMenuOutline onClick={toggleMenu} className={`block sm:hidden cursor-pointer text-2xl ${stickyNavbar ? 'text-customeGreen' : 'text-customeWhite'}`} />
         </nav>
     );
 };
